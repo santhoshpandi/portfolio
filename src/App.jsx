5644674aa3cf3beda5282d9b346933d71d05d759
weber <santhoshpandi543@gmail.com>
2025-03-10 18:02:30 +0530

    minor-updation

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
   return (
     <>
-      {/* <SplashCursor /> */}
+      <SplashCursor />
       <div className='fixed top-0 left-0 right-0 bottom-0 z-[-1]'>
         <ReactNebula config={{
           starsCount: 100,
