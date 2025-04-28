11161bb75abd85147b25b69813c3611ee1fea829
santhoshpandi <santhoshpandi543@gmail.com>
2025-04-28 11:18:12 +0530

    audio added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { ReactNebula } from "@flodlc/nebula";
-import { Suspense, lazy } from 'react'
+import { Suspense, lazy, useEffect } from 'react'
 import Loader from './components/minorComponents/Loader';
 
 const Header = lazy(() => import('./components/Header'))
@@ -12,9 +12,19 @@ const Contact = lazy(() => import('./components/Contact'))
 const Projects = lazy(() => import('./components/Projects'))
 const Certification = lazy(() => import('./components/Certification'))
 import SplashCursor from './components/minorComponents/SplashCursor'
+import { use } from 'react';
 
 function App() {
 
+  useEffect(() => {
+    window.addEventListener('click', () => {
+      const audio = document.getElementById('audio');
+      audio.muted = false;
+      audio.volume = 0.2
+      audio.play().catch(console.error); // replays in case it stopped
+    }, { once: true });
+  }
+  , [])
 
   return (
     <>
@@ -31,6 +41,7 @@ function App() {
         }} />
       </div>
       <Suspense fallback={<Loader />}>
+        <audio id='audio' src="/portfolio/assets/audio.mp3" muted autoPlay loop />
         <Header />
         <Home />
         <About />
