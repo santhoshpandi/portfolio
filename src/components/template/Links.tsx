5703cdb420a40211a7e966fa6f5bbb4a2711163e
santhoshpandi <santhoshpandi543@gmail.com>
2026-03-19 13:34:12 +0530

    feat: add more social media links support

--- src/components/template/Links.tsx
+++ src/components/template/Links.tsx
@@ -1,11 +1,39 @@
 import { IoLogoLinkedin, IoLogoInstagram, IoLogoGithub } from "react-icons/io5";
+import { FaFacebook, FaDiscord } from "react-icons/fa";
+import { FaXTwitter, FaYoutube } from "react-icons/fa6";
+import { SiLeetcode } from "react-icons/si";
 import data from "../../data/data.json"
+
 export default function Links() {
+
+  const { links } = data
+
+  const iconList = [
+    { name: "linkedin", icon: <IoLogoLinkedin />, color: 'text-blue-400' },
+    { name: "github", icon: <IoLogoGithub />, color: 'text-white' },
+    { name: "instagram", icon: <IoLogoInstagram />, color: 'text-pink-400' },
+    { name: "leetcode", icon: <SiLeetcode />, color: 'text-orange-400' },
+    { name: "facebook", icon: <FaFacebook />, color: 'text-blue-600' },
+    { name: "discord", icon: <FaDiscord />, color: 'text-violet-400' },
+    { name: "x", icon: <FaXTwitter />, color: 'text-white' },
+    { name: "youtube", icon: <FaYoutube />, color: 'text-red-500' },
+  ]
+
   return (
-    <>
-      <li><a target="_blank" className='text-blue-300' href={data.links[0].url}><IoLogoLinkedin /></a></li>
-      <li><a target="_blank" className='text-pink-400' href={data.links[1].url}><IoLogoInstagram /></a></li>
-      <li><a target="_blank" className='text-white' href={data.links[2].url}><IoLogoGithub /></a></li>
-    </>
+    <div className="flex gap-3 justify-between items-center flex-wrap">
+      {
+        links.map((link, index) => {
+          const icon = iconList.find(icon => icon.name === link.name.toLowerCase().trim())
+
+          if (icon) {
+            return (<li key={index}>
+              <a target="_blank"
+                className={`${icon.color}`}
+                href={data.links[1].url}>{icon.icon}</a>
+            </li>)
+          }
+        })
+      }
+    </div>
   )
 }
